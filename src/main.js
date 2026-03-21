ee3f36a7585bcb6650bb0ad05e089b0569e89149
Taylor Parker <taylorparkerr7@gmail.com>
2026-03-21 11:48:19 -0500

    Added mapbox API and map to index.html

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+const token = import.meta.env.VITE_MAPBOX_TOKEN;
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-86.8025, 33.4018],
+  zoom: 11
+});
+
 class App {
     constructor(){
         this.availability = document.getElementById('availability');
         this.wait = document.getElementById('wait-time');
     }
-
-    
 }
